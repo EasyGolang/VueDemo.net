9818107be164b7983e44c1aec5d9540144b56eaa
墨七 <meichangliang@mo7.cc>
2022-06-14 22:02:04 +0800

    exit-push

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,15 +1,36 @@
+<script setup lang="ts">
+import { getToken, mStorage } from '@/utils/tools';
+import { outLogin } from '@/api/Account';
+const token = getToken();
+const UserInfo = mStorage.get('user_info');
+
+const handleOutLogin = () => {
+  outLogin();
+};
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div v-if="token">
+      <br />
+      <img class="Avatar" :src="UserInfo.Avatar" alt="" srcset="" />
+      <br />
+      欢迎访问,{{ UserInfo.NickName }}
+      <br />
+      <button @click="handleOutLogin">退出登录</button>
+    </div>
+    <div v-if="!token">
+      <br />
+      <a href="/login"><button>请登录</button></a>
+      <br />
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style lang="less">
+.Avatar {
+  width: 100px;
+  height: 100px;
+  border-radius: 100px;
 }
 </style>
